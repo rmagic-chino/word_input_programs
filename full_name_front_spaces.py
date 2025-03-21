0e32c1381b64d82c1be4b89c6869f3901595d5b2
Carlos De Guzman <raremagic01@gmail.com>
2025-03-21 23:33:17 +0800

    print name ignoring leading spaces

--- full_name_front_spaces.py
+++ full_name_front_spaces.py
@@ -1,6 +1,12 @@
 #define command
+def main():
 
    #set rules and conditions
+    full_name = input("Enter name with leading spaces: ")
+    trimmed_name = full_name.lstrip()
+    print("Output: ", trimmed_name)
    #print output
 
-#execute
+if __name__ == "__main__":
+    main()
+   #execute
