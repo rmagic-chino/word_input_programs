a2ceb499133487ec6b60c92ef02b8450339c1e95
Carlos De Guzman <raremagic01@gmail.com>
2025-03-22 07:19:12 +0800

    print output in snake snake case

--- snake_case_output.py
+++ snake_case_output.py
@@ -1,7 +1,12 @@
 #define
+def main():
 
     #input name
-    #make conditions for snake case
+    full_name = input("Enter name: ")
+    snake_case = full_name.lower().replace(" ", "_")
+    print("Output: ", snake_case)
     #print output
 
+if __name__ == "__main__":
+    main()
 #execute
