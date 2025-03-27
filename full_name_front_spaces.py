9a955026756f634a74ff6bf580dfa8dc2b023e63
Carlos De Guzman <raremagic01@gmail.com>
2025-03-27 10:39:17 +0800

    print full_name_front_spaces(updated)

--- full_name_front_spaces.py
+++ full_name_front_spaces.py
@@ -1,12 +1,5 @@
-#define command
-def main():
-
-   #set rules and conditions
-    full_name = input("Enter name with leading spaces: ")
-    trimmed_name = full_name.lstrip()
-    print("Output: ", trimmed_name)
-   #print output
-
-if __name__ == "__main__":
-    main()
-   #execute
+#set rules and conditions
+full_name = input("Enter name with leading spaces: ")
+trimmed_name = full_name.lstrip()
+print("Output: ", trimmed_name)
+#print output
