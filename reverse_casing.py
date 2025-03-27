4815e5bf4fc64fc6cf4de897858cb5c7a0ee1acf
Carlos De Guzman <raremagic01@gmail.com>
2025-03-27 10:55:51 +0800

    print reverse casing(updated)

--- reverse_casing.py
+++ reverse_casing.py
@@ -1,12 +1,5 @@
-#define
-def main():
-
-    #input name
-    full_name = input("Enter name: ")
-    new_name = full_name.swapcase()
-    print("Output: ", new_name)
-    #print result
-
-if __name__ == "__main__":
-    main()
-#execute command
+#input name
+full_name = input("Enter name: ")
+new_name = full_name.swapcase()
+print("Output: ", new_name)
+#print result
