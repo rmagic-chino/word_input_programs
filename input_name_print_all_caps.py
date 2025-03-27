ee76833c533ab103d1a73a061b2596dc83a68e31
Carlos De Guzman <raremagic01@gmail.com>
2025-03-27 10:40:38 +0800

    print name all caps(updated)

--- input_name_print_all_caps.py
+++ input_name_print_all_caps.py
@@ -1,12 +1,5 @@
-#define command
-def main():
-
-    #input name
-    full_name = input("Enter name: ")
-    all_caps = full_name.upper()
-    print("Output: ", all_caps)
-    #print result
-
-if __name__ == "__main__":
-    main()
-#execute
+#input name
+full_name = input("Enter name: ")
+all_caps = full_name.upper()
+print("Output: ", all_caps)
+#print result
