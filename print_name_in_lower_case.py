8d513b83e3dc17f79d85408645b01b746d693b7d
Carlos De Guzman <raremagic01@gmail.com>
2025-03-27 10:50:08 +0800

    print in low caps(updated)

--- print_name_in_lower_case.py
+++ print_name_in_lower_case.py
@@ -1,12 +1,5 @@
-#define
-def main():
-
-    #input name
-    full_name = input("Enter name: ")
-    lower_case = full_name.lower()
-    print("Output: ", lower_case)
-    #print output
-
-if __name__ == "__main__":
-    main()
-#execute
+#input name
+full_name = input("Enter name: ")
+lower_case = full_name.lower()
+print("Output: ", lower_case)
+#print output
