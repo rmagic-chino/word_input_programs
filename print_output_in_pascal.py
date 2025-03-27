16f633ec314ba148e05ef62b6ed65956019e716c
Carlos De Guzman <raremagic01@gmail.com>
2025-03-27 10:54:32 +0800

    print pascal(updated)

--- print_output_in_pascal.py
+++ print_output_in_pascal.py
@@ -1,12 +1,5 @@
-#define
-def main():
-
-    #input name
-    full_name = input("Enter name: ")
-    pascal_casing = full_name.title().replace(" ", "")
-    print("Output: ", pascal_casing)
-    #print output
-
-if __name__ == "__main__":
-    main()
-#execute
+#input name
+full_name = input("Enter name: ")
+pascal_casing = full_name.title().replace(" ", "")
+print("Output: ", pascal_casing)
+#print output
