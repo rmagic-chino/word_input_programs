36c9cef776fac3c3745f1b223a183129fe1eaee0
Carlos De Guzman <raremagic01@gmail.com>
2025-03-27 10:52:04 +0800

    print proper casing(updated)

--- print_name_in_proper_casing.py
+++ print_name_in_proper_casing.py
@@ -1,12 +1,5 @@
-#define
-def main():
-
-    #input name
-    full_name = input("Enter name in random casing: ")
-    proper = full_name.title()
-    print("Output: ", proper)
-    #print result
-
-if __name__ == "__main__":
-    main()
-#execute
+#input name
+full_name = input("Enter name in random casing: ")
+proper = full_name.title()
+print("Output: ", proper)
+#print result
