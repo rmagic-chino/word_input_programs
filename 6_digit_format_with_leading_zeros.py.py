fe6e076ed2ce3948747f34cffff7ab3888c6dcd2
Carlos De Guzman <raremagic01@gmail.com>
2025-03-27 10:31:57 +0800

    6 digit format with leading zeros(updated)

--- 6_digit_format_with_leading_zeros.py.py
+++ 6_digit_format_with_leading_zeros.py.py
@@ -1,12 +1,5 @@
-#define command
-def main():
-
-    #input number
-    number = input("Enter number (0-1000): ")
-    formatted_number = f"{int(number):06d}"
-    print("Output: ", formatted_number)
-    #print output
-
-if __name__ == "__main__":
-    main()
-#execute
+#input number
+number = input("Enter number (0-1000): ")
+formatted_number = f"{int(number):06d}"
+print("Output: ", formatted_number)
+#print output
