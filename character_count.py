6e2518cff925efc9358bec458a1fce90f94dc833
Carlos De Guzman <raremagic01@gmail.com>
2025-03-27 10:33:47 +0800

    character_count(updated)

--- character_count.py
+++ character_count.py
@@ -1,12 +1,5 @@
-#define
-def main():
-
-    #input any word
-    sentence = input("Enter a sentence/statement: ")
-    character_count = len(sentence.replace(" ", ""))
-    print("Output: ", character_count)
-    #print output
-
-if __name__ == "__main__":
-    main()
-#execute
+#input any word
+sentence = input("Enter a sentence/statement: ")
+character_count = len(sentence.replace(" ", ""))
+print("Output: ", character_count)
+#print output
